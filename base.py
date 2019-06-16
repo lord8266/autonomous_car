5afb421628e38c284acef7c87e97220e0b35e455
lord8266 <lordvishwa123@gmail.com>
2019-06-16 18:02:53 +0530

    Addedcode to work with a learning model

--- base.py
+++ base.py
@@ -6,31 +6,54 @@ import route
 import pygame
 import game_loop
 import VehicleController
+import Simulator
+# client = carla.Client('127.0.0.1',2000)
+# client.set_timeout(2.0)
+# world = client.get_world()
 
-client = carla.Client('127.0.0.1',2000)
-client.set_timeout(2.0)
-world = client.get_world()
+# settings = world.get_settings() # no render
+# settings.no_rendering_mode = False
+# world.apply_settings(settings)
 
-settings = world.get_settings() # no render
-settings.no_rendering_mode = False
-world.apply_settings(settings)
+# world_map = world.get_map()
+# all_points = world_map.get_spawn_points()
+# start,end = all_points[0],all_points[1]
 
-world_map = world.get_map()
-all_points = world_map.get_spawn_points()
-start,end = all_points[0],all_points[1]
+# vehicle,cam =  VehicleController.init_vehicle(world,start)
+# controller = VehicleController.VehicleController(vehicle)
+# route = route.Route(world,vehicle)
+# route.make_route(start,end,6)
+# route.draw_path()
+# g = game_loop.GameLoop(cam,controller,route)
 
-vehicle,cam =  VehicleController.init_vehicle(world,start)
-controller = VehicleController.VehicleController(vehicle)
-route = route.Route(world,vehicle)
-route.make_route(start,end,6)
-route.draw_path()
-g = game_loop.GameLoop(cam,controller,route)
+simulator = Simulator.Simulator()
+simulator.init_system()
+# g.run()
 
+running = simulator.game_loop.running
+prev_state = None
+prev = pygame.time.get_ticks()
+curr_reward =0 
+clock = pygame.time.Clock()
+while running:
+    clock.tick_busy_loop(60)
+    step =0
+    if prev_state!=None:
+        step = 0
+    
+    state,reward,done = simulator.step(0)
+    prev_state = state
+    curr_reward+=reward
+    curr = pygame.time.get_ticks()
 
-g.run()
+    if done:
+        simulator.reset()
+        print(curr_reward)
+        curr_reward = 0
+    simulator.render()
+    running = simulator.game_loop.running
 
-cam.stop()
-vehicle.destroy()
+simulator.stop()
 pygame.quit()
 
 
