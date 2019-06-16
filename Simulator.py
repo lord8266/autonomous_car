b53223f65668f7452ab1c4169680d29c074a29bf
lord8266 <lordvishwa123@gmail.com>
2019-06-16 14:53:54 +0530

    Added gym like functons

--- Simulator.py
+++ Simulator.py
@@ -9,6 +9,7 @@ import RewardSystem
 class Simulator:
 
     def __init__(self):
+        pygame.init()
         self.intitalize_carla()
 
     def intitalize_carla(self):
@@ -18,6 +19,7 @@ class Simulator:
 
         settings = world.get_settings() # no render
         settings.no_rendering_mode = False
+        settings.synchronous_mode = True
         world.apply_settings(settings)
 
         world_map = world.get_map()
@@ -25,17 +27,38 @@ class Simulator:
         start,end = all_points[0],all_points[1]
 
         vehicle,cam =  VehicleController.init_vehicle(world,start)
-        controller = VehicleController.VehicleController(vehicle)
-        route = route.Route(world,vehicle)
-        route.make_route(start,end,6)
-        route.draw_path()
-        g = game_loop.GameLoop(cam,controller,route)
+        controller = VehicleController.VehicleController(vehicle,AI=True)
+        route_ = route.Route(world,vehicle)
+        route_.make_route(start,end,6)
+        route_.draw_path()
+        g = game_loop.GameLoop(cam,controller,route_)
 
         self.client = client
         self.world = world
         self.world_map =world_map
         self.start =start
         self.end =end
-        self.route = route
+        self.route = route_
         self.game_loop = g
-        
+        self.reward_system = RewardSystem.RewardSystem(end)
+        self.controller = controller
+
+    def step(self,action): # action is a steer angle from -0.5 to 0.5 (steer)
+        self.controller.control_by_AI(action)
+        self.client.tick()
+        data = self.route.get_dynamic_path()
+        self.reward_system.update_data(data[1],data[2])
+        done,reward = self.reward_system.update_rewards()
+        return data[0],reward,done
+    
+    def reset(self):
+        self.controller.reset(self.start)
+        self.reward_system.reset()
+    
+    def init_system(self):
+        self.reset()
+        self.client.tick()
+    
+    def render(self):
+        self.game_loop.run() # temporary
+
