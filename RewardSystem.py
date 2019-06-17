9d1108acd75d7acd56db6663b390d2132e4f1706
lord8266 <lordvishwa123@gmail.com>
2019-06-17 23:03:24 +0530

    Added checkpoint_reward

--- RewardSystem.py
+++ RewardSystem.py
@@ -2,7 +2,7 @@ import carla
 import route
 class RewardSystem:
 
-    def __init__(self,end_transform):
+    def __init__(self,end_transform,route_):
         RewardSystem.ref  = self
         self.actor_transform = None
         self.waypoint_transform = None
@@ -10,7 +10,8 @@ class RewardSystem:
         self.max_rot_offset = 50/180
         self.done = False
         self.end_transform = end_transform
-
+        self.route = route_
+        self.prev_pos = route_.curr_pos
     def update_data(self,actor_transform,waypoint_transform):
         self.actor_transform = actor_transform
         self.waypoint_transform = waypoint_transform
@@ -27,14 +28,25 @@ class RewardSystem:
         
     def set_distance_reward(self):
         
-        d = route.Route.get_distance(self.waypoint_transform.location,self.actor_transform.location,res=0)
+        d = route.Route.get_distance(self.waypoint_transform.location,self.actor_transform.location,res=1)
         self.d = d
-        if d>=2.7:
+        if d>=1:
             self.done = True
             print(f'done as distance is {d}')
         else:
             self.curr_reward += -d*30 # temporary
 
+    def checkpoint_reward(self):
+        pos = self.route.curr_pos
+        if pos>self.prev_pos:
+            self.curr_reward+=20
+        elif pos<self.prev_pos:
+            self.curr_reward -=-50
+        else:
+            self.curr_reward -= 5.6
+            
+        self.prev_pos = pos
+            
     def update_rewards(self):
         self.curr_reward =0
         self.set_distance_reward()
